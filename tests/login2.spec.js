3f920a5ce649725c9d7c847ff92ca009ddf4b68b
Shahealam <shahealam256@gmail.com>
2026-05-26 23:18:38 +0530

    fixed duplicate test title

--- tests/login2.spec.js
+++ tests/login2.spec.js
@@ -8,14 +8,14 @@ test('verify console message',async({page})=>{
 );
 
 
-test('verify console message',async({page})=>{
+test('verify console message1',async({page})=>{
 
     console.log('su branch testing');
 }
 
 );
 
-test('verify console message',async({page})=>{
+test('verify console message2',async({page})=>{
 
     console.log('su branch testing');
 }
